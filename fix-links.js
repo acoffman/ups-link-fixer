edd4076b00cac8556d5e74b727dda06ff32419b6
Adam Coffman <acc@fastmail.com>
2020-11-17 16:43:25 -0600

    wrap everything to avoid redefinition errors

--- fix-links.js
+++ fix-links.js
@@ -1,10 +1,12 @@
-const trackingNumberRegex = new RegExp('(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)');
-const selector = 'a[alt="Tracking Number"]';
-const trackingUrlTemplate = 'https://www.ups.com/track?tracknum=';
-const myChoiceLinkContent = "upsmychoice";
+(() => {
+  const trackingNumberRegex = new RegExp('(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)');
+  const selector = 'a[alt="Tracking Number"]';
+  const trackingUrlTemplate = 'https://www.ups.com/track?tracknum=';
+  const myChoiceLinkContent = "upsmychoice";
 
-let openLinks = () => Array.from(document.querySelectorAll(selector))
-    .filter(node => node.href.includes(myChoiceLinkContent) && trackingNumberRegex.test(node.text))
-    .forEach(node => window.open(trackingUrlTemplate + node.text, '_blank'));
+  const openLinks = () => Array.from(document.querySelectorAll(selector))
+      .filter(node => node.href.includes(myChoiceLinkContent) && trackingNumberRegex.test(node.text))
+      .forEach(node => window.open(trackingUrlTemplate + node.text, '_blank'));
 
-openLinks();
+  openLinks();
+})();
